3344c9b56ce3c5f7f31a5b62aa44b04e3e3533f9
sudhahcl <61785776+sudhahcl@users.noreply.github.com>
2021-03-11 17:43:03 +0530

    Add files via upload
    edit changes

--- src/editPayee.js
+++ src/editPayee.js
@@ -1,5 +1,8 @@
+// Get url params
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const id = urlParams.get('id');
 // Get edit data
-let id = 1;
 let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.status == 200) {
@@ -20,3 +23,30 @@ myEditFunc=(myArr)=>{
     document.getElementById("bankName").value = myArr[0].bankName;
     document.getElementById("code").value = myArr[0].mobile;
     }
+
+// Update data into json server
+onEditFormSubmit=() =>{
+    let formData={};
+    formData["name"]=document.getElementById("name").value;
+    formData["accountNumber"]=document.getElementById("accountNumber").value;
+    formData["bankName"]=document.getElementById("bankName").value;
+    formData["code"]=document.getElementById("code").value;
+    let allData = JSON.stringify(formData);  
+    xhttp.open("PUT", "http://localhost:3000/payee/"+id, true );
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.send(allData);
+    window.location.href = "http://localhost:5500/public/payeeList.html";    
+    alert("Updated successfully");
+
+}
+/*to delete the record*/
+function onDelete(){
+    console.log(id)
+    // if (confirm('Are you sure to delete this record ?')) {     
+    //     fetch("http://localhost:3000/payee/"+id,{
+    //             method:"DELETE"
+    //         })
+    //     }   
+    }     
+
+    
